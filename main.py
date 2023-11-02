f4ef72b1218076ca15efacd94857450b2f1cac34
xdCranx <264257@student.pwr.edu.pl>
2023-11-02 14:27:38 +0100

    Added MNIST data sets and first steps fo the project

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+# 1. Utworzenie/wprowadzenie do baz przykładów uczących
+
+# 2. Zdefiniowanie rozmiaru sieci
+
+# 3. Ustawienie parametrów uczenia się sieci
+
+# 4. Inicjalizacja wstępnej macierzy wag
