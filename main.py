b5d4d603d88b66287f35765aabd90e4213d89dd3
AlekPajor <aciualeksander@gmail.com>
2023-11-02 18:10:59 +0100

    Base version

--- main.py
+++ main.py
@@ -1,51 +1,26 @@
-
-# 1. Utworzenie/wprowadzenie do baz przykładów uczących
-
-# 2. Zdefiniowanie rozmiaru sieci
-
-# 3. Ustawienie parametrów uczenia się sieci
-
-# 4. Inicjalizacja wstępnej macierzy wag
-
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import pyplot as plt
 
+data = pd.read_csv('mnist_train.csv')
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data)
 
-def show_letters():
-    # Load data from the CSV file
-    csv_file = './MNIST_data/letters_data_set.csv'
-    data = np.genfromtxt(csv_file, delimiter=',', dtype=int)
+data2 = pd.read_csv('mnist_test.csv')
+data2 = np.array(data2)
+np.random.shuffle(data2)
 
-    # Reshape the data into 7x5 arrays
-    num_images, image_height, image_width = data.shape[0], 7, 5
-    images = data.reshape(num_images, image_height, image_width)
+data_dev = data2.T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
 
-    # Display the images
-    fig, axes = plt.subplots(1, num_images, figsize=(num_images * 2, 2))
-
-    for i in range(num_images):
-        axes[i].imshow(images[i], cmap='gray')
-        axes[i].set_title(f'Image {i + 1}')
-        axes[i].axis('off')
-
-    plt.tight_layout()
-    plt.show()
-
-
-def load_letters_set():
-    data = np.genfromtxt('MNIST_data/letters_data_set.csv', delimiter=',', dtype=int)
-    return data
-
-
-def load_train_set():
-    data = np.genfromtxt('MNIST_data/mnist_train.csv', delimiter=',', dtype=int)
-    return data
-
-
-def load_test_set():
-    data = np.genfromtxt('MNIST_data/mnist_test.csv', delimiter=',', dtype=int)
-    return data
-
+data_train = data.T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_, m_train = X_train.shape
 
 def init_params():
     W1 = np.random.rand(10, 784) - 0.5
@@ -55,4 +30,98 @@ def init_params():
     return W1, b1, W2, b2
 
 
-load_train_set()
+def ReLU(Z):
+    return np.maximum(Z, 0)
+
+
+def softmax(Z):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+
+def ReLU_deriv(Z):
+    return Z > 0
+
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+
+test_prediction(0, W1, b1, W2, b2)
+test_prediction(1, W1, b1, W2, b2)
+test_prediction(2, W1, b1, W2, b2)
+test_prediction(3, W1, b1, W2, b2)
